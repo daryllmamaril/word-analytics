0a118386bc8c7f3acf9a0112119a0a38010063af
daryll.mamaril <daryll.mamaril@avanade.com>
2024-07-25 21:59:14 +1000

    Add files for the basic components the project needs

---
+++ src/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer>
+      <small>&copy; Copyright by ByteGrad. All rights reserve.</small>
+      <small>Last checked limits: 17 days ago.</small>
+    </footer>
+  )
+}
---
+++ src/H1.jsx
@@ -0,0 +1,5 @@
+export default function H1() {
+  return (
+    <h1>Word Analytics</h1>
+  )
+}
