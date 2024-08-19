053783a1720deb44994b37da654467a13fd12377
daryll.mamaril <daryll.mamaril@avanade.com>
2024-08-19 15:40:43 +1000

    Word countess app completed

--- src/Footer.jsx
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="footer">
-      <small>&copy; Copyright by Dazz Mamaril. All rights reserve.</small>
-      <small>Last checked limits: 17 days ago.</small>
-    </footer>
-  )
-}
---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <small>&copy; 2024 Dazz Mamaril.</small>
+      <small>Melbourne, Australia. All rights reserve.</small>
+    </footer>
+  )
+}
